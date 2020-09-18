5a4366f09b68bd7bee6865dd37a6715e00e3a920
Wagner Paulino <wagnersantos9778@gmail.com>
2020-09-17 22:21:51 -0300

    filtragem basica

--- meusvideos-service/meusvideosbackend/meusvideos/views.py
+++ meusvideos-service/meusvideosbackend/meusvideos/views.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response
+import django_filters.rest_framework
 from rest_framework import viewsets
 from meusvideosbackend.meusvideos.models import Usuario, Video, Resenha
 from meusvideosbackend.meusvideos.serializers import UsuarioSerializer, VideoSerializer, ResenhaSerializer
@@ -10,6 +10,8 @@ class UsuarioViewSet(viewsets.ModelViewSet):
     """
     queryset = Usuario.objects.all().order_by('id')
     serializer_class = UsuarioSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = '__all__'
 
 
 class VideoViewSet(viewsets.ModelViewSet):
@@ -18,6 +20,8 @@ class VideoViewSet(viewsets.ModelViewSet):
     """
     queryset = Video.objects.all().order_by('id')
     serializer_class = VideoSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = '__all__'
 
     def get_object(self):
         queryset = self.filter_queryset(self.get_queryset())
@@ -30,3 +34,5 @@ class ResenhaViewSet(viewsets.ModelViewSet):
     """
     queryset = Resenha.objects.all().order_by('id')
     serializer_class = ResenhaSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = '__all__'
